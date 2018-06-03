9ee906697751df42c2e2c55753147d34897d119b
Mike Hernandez <mkhern@gmail.com>
2018-06-03 11:24:52 -0700

    Corrects data flow after selecting a new date. Date props now in sync with state.

--- src/client/News/News.jsx
+++ src/client/News/News.jsx
@@ -7,14 +7,14 @@ class News extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      keyword: this.props.keyword,
-      date: this.props.date,
+      keyword: null,
+      date: null,
       data: [],
     }
   }
   
   componentDidMount() {
-    const { keyword, date } = this.state;
+    const { keyword, date } = this.props;
     this._fetchNewsForDate(keyword, date);
   }
 
@@ -22,37 +22,38 @@ class News extends Component {
     fetch(`/api/news?keyword=${keyword}&date=${date}`)
       .then(res => res.json())
       .then(data => {
-        this.setState({ data: data, keyword: keyword });
+        this.setState({ keyword: keyword, date: date, data: data });
       });
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
+  shouldComponentUpdate(nextProps) {
     if (this.state.data.length === 0) {
       return true;
     }
 
-    if (nextProps.date !== nextState.date) {
+    if (this.props.date !== this.state.date) {
       return true;
     }
 
-    if (nextProps.keyword !== nextState.keyword) {
+    if (nextProps.date !== this.state.date) {
       return true;
     }
-    
+
     return false;
   }
 
-  componentDidUpdate(prevProps) {
-    if (this.props.date !== prevProps.date) {
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.date !== prevState.date) {
       this._fetchNewsForDate(this.props.keyword, this.props.date);
     }
   }
 
   render() {
-    const articles = (this.state.data.news !== undefined) ? this.state.data.news.articles : [];
-    const headlines = articles.map(({ title, url }) => (
-      <NewsHeadline headline={title} key={url} />
-    )).slice(0, 4);
+    const articles = this.state.data.news !== undefined ? this.state.data.news.articles : [];
+    const headlines = articles
+      .map(({ title, url }) => <NewsHeadline headline={title} key={url} />)
+      .slice(0, 4);
+
     return(
       <div className="newsWrapper">
         <CSSTransition in={(headlines.length > 0)} timeout={2500} classNames="initHeadline">
