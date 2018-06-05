25898104a9551c43becdad3acdbcbb4e9b887e66
Mike Hernandez <mkhern@gmail.com>
2018-06-05 14:45:47 -0700

    Displays article source and time.

--- src/client/News/NewsDetail.jsx
+++ src/client/News/NewsDetail.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import NewsDetailPreview from './NewsDetailPreview';
 import NewsDetailImage from './NewsDetailImage';
+import NewsDetailMeta from './NewsDetailMeta';
 
 const NewsDetail = (props) => {
   const article = props.article[0];
 
   return(
     <div className="news-detail--wrapper">
-      <div className="top">top</div>
+      <NewsDetailMeta sourceName={article.source.name} date={article.publishedAt} />
       <NewsDetailPreview content={article.description} />
       <NewsDetailImage image={article.urlToImage} />
     </div>
