a35ca67cc83a28c49fc4dd093d6e536fc6983275
Mike Hernandez <mkhern@gmail.com>
2018-06-18 14:17:11 -0700

    Landing page fades out upon search submission.

--- src/client/App/App.jsx
+++ src/client/App/App.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import LandingPage from '../LandingPage/LandingPage';
 import Dashboard from '../Dashboard/Dashboard';
-
-import './app.css';
+import { Transition } from 'react-transition-group';
 
 class App extends Component {
   constructor(props) {
@@ -27,7 +26,6 @@ class App extends Component {
   }
 
   _setSearch(term) {
-    console.log('term is: ', term);
     this.setState({
       landingSearchTerm: term,
       landingSearchSubmitted: true,
@@ -37,6 +35,13 @@ class App extends Component {
   render() {
     const {landingLoadFinished, landingSearchSubmitted, landingSearchTerm } = this.state;
     const tempImage = "https://images.pexels.com/photos/660548/bormio-river-pebbles-italy-660548.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=1142&w=1920";
+    const transitionStyles = {
+      entering: {
+        opacity: 0,
+        transition: `opacity 2000ms cubic-bezier(0.215, 0.610, 0.355, 1.000)`
+      },
+      entered: { display: 'none' },
+    }
 
     return (
       <React.Fragment>
@@ -45,8 +50,13 @@ class App extends Component {
             src={tempImage}
             style={{ display: 'none' }}
             onLoad={this._imageLoadComplete} />}
-        {landingLoadFinished && !landingSearchSubmitted &&
-          <LandingPage landingSearchTerm={(term) => this._setSearch(term)} />}
+          <Transition in={(landingLoadFinished && landingSearchSubmitted)} timeout={5000}>
+            {(state) => (
+              <div style={transitionStyles[state]}>
+                <LandingPage landingSearchTerm={(term) => this._setSearch(term)} />
+              </div>
+            )}
+          </Transition>
         {landingSearchSubmitted && <Dashboard landingSearch={landingSearchTerm}/>}
       </React.Fragment>
     );
